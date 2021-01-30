8e4987a22377f6c472fbd84d20a6afa62a795475
Nidvar <djayshine87@hotmail.com>
2021-01-30 11:25:27 +1100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pwgen';
+
+  useNumbers = false;
+  useLetters = false;
+  useSymbols = false;
+  password=''
+
+  onUseNumberCheck(){
+    this.useNumbers = !this.useNumbers
+    console.log(this.useNumbers)
+  }
+
+  onUseLettersCheck(){
+    this.useLetters = !this.useLetters
+    console.log(this.useLetters)
+  }
+
+  onUseSymbolsCheck(){
+    this.useSymbols = !this.useSymbols
+    console.log(this.useSymbols)
+  }
+
+  onButtonClick(){
+    this.password = 'super genius'
+  }
+
+  getPassword(){
+    return this.password
+  }
 }
